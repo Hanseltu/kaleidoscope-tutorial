2eb3452d4e5125ddea47f643ab404a80860127ab
Hanseltu <tuhaoxin6@gmail.com>
2019-10-03 22:20:49 +0800

    master

---
+++ clion-project/kaleidoscope/cmake-build-debug/CMakeFiles/kaleidoscope.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kaleidoscope.dir/chapter2-Implementing-a-Parser-and-AST.cpp.o"
+  "kaleidoscope.pdb"
+  "kaleidoscope"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/kaleidoscope.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
